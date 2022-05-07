3adab0fdcac148d6c627f3ad21a6de47c93cbc61
Alex Shyba <57730703+uniform-demo@users.noreply.github.com>
2022-05-07 16:25:19 -0700

    fix: json parse when cart 404s

--- src/pages/api/cart.ts
+++ src/pages/api/cart.ts
@@ -123,9 +123,12 @@ const handleGet = async (req: NextApiRequest, res: NextApiResponse) => {
     headers: bigCommerceRequestHeaders,
   });
 
-  const json = await response.json();
-
-  res.status(response.status).json(json);
+  try {
+    const json = await response.json();
+    res.status(response.status).json(json);
+  } catch (error) {
+    console.log('cart is empty');
+  }
 };
 
 const handlePost = async (req: NextApiRequest, res: NextApiResponse) => {
