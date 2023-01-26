550a5c78b4a66b89b89b64178afd71b7c7645271
Ante Sepic <antesepic@protonmail.com>
2023-01-26 14:55:05 +0100

    fix: provide correct value for crossOrigin attribute

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link
             href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,500&display=swap"
             rel="stylesheet"
